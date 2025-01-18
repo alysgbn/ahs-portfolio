9c8cef7b3503e154894e4ea180a2206cf31818f4
alysgbn <aliyah.sagaban22@gmail.com>
2025-01-18 19:13:01 +0800

    Added fonts and tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="hero-page  ">
+      {/* <header className="App-header">
+        <p className="text-white">Aliyah Sagaban</p>
+        <p> Test 13245</p>
+        <p> Test ulit</p>
+      </header> */}
+      <p>Aliyah Sagaban</p>
+    </div>
+  );
+}
+
+export default App;
