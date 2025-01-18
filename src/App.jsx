a1f704ba43f30af7eb38f19cbe86756fd3214458
alysgbn <aliyah.sagaban22@gmail.com>
2025-01-18 21:16:47 +0800

    Added SASS and Button NEXT UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
-import "./App.css";
+import "./App.scss";
 import MyNavbar from "./components/Navbar";
+
+import myAvatar from "./assets/images/avatar-laptop.png";
+import myAvatarLike from "./assets/images/avatar-like.png";
+import { Button } from "@heroui/button";
 function App() {
   return (
     <>
       <MyNavbar className="w-100" />
       <div className="hero-page">
-        <p>Aliyah Sagaban</p>
+        <img src={myAvatar} alt="" />
+        <div className="hero-content">
+          <h1>Aliyah Sagaban</h1>
+          <p>
+            A Computer Science graduate, major in Data Science. I am an aspiring
+            Data Engineer
+          </p>
+
+          <div className="hero-buttons">
+            <Button
+              className=" text-white shadow-lg"
+              radius="full"
+              variant="bordered"
+              fullWidth={true}
+            >
+              About Me
+            </Button>
+
+            <Button
+              className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg"
+              fullWidth={true}
+              radius="full"
+            >
+              Contact Me
+            </Button>
+          </div>
+        </div>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "./node_modules/@heroui/theme/dist/components/navbar.js",
+    "./node_modules/@heroui/theme/dist/components/button.js",
   ],
   theme: {
     extend: {},
