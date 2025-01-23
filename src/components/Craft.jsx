e07902bbda391606524bc3466ffb4dfb195dd0ae
Aliyah Sagaban <aliyah.sagaban22@gmail.com>
2025-01-23 17:45:50 +0800

    Initial commit: Craft

---
+++ src/components/Craft.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import '../assets/css/craft.scss'
+import { motion } from "framer-motion";
+const Craft = () => {
+
+    return (
+        <div className='craft-section'>
+
+            <div className="craft-header">
+                <motion.div
+                    initial={{ opacity: 0, y: 100 }}
+                    whileInView={{
+                        opacity: 1,
+                        y: 0,
+                        transition: {
+                            duration: 0.6,
+                            delay: 0.2,
+                            ease: "easeInOut",
+                        },
+                    }}
+                >
+                    <div className="journey-header">
+                        <p>CRAFTS</p>
+                        <h1>Data Science</h1>
+                    </div>
+                </motion.div>
+            </div>
+        </div>
+    )
+}
+
+export default Craft;
